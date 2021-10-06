05081c0b299e8c0a91b5f69f043465a15bdb984a
JamieWilson2000 <jamiewilson2000@yahoo.com>
2021-10-06 10:47:40 +0000

    Added checkout views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,7 +1,6 @@
 """boutique_ado URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -24,4 +23,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
